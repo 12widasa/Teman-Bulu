216256d1a1da6f21e2743230fa17f1b0d650d4cc
Franscisco <fransciscoangga123@gmail.com>
2025-05-26 22:35:16 +0700

    add other pages

--- teman-bulu-fe/src/App.jsx
+++ teman-bulu-fe/src/App.jsx
@@ -2,14 +2,33 @@ import { useState } from 'react'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './App.css'
 import Login from './Pages/Auth/Login'
-import Dashboard from './Pages/Admin/Dashboard';
 import LandingPage from './Pages/LandingPage/LandingPage';
 import RegisterBuyer from './Pages/Auth/RegisterBuyer';
 import RegisterSeller from './Pages/Auth/RegisterSeller';
-
+import DataBuyer from './Pages/Admin/DataBuyer';
+import DataSeller from './Pages/Admin/DataSeller';
+import PageNotFound from './Pages/PageNotFound/PageNotFound';
+import VerificationData from './Pages/Admin/VerificationData';
+import PesanLayanan from './Pages/Pembeli/PesanLayanan/PesanLayanan';
+import DetailPesanan from './Pages/Pembeli/DetailPesanan/DetailPesanan';
+import Pembayaran from './Pages/Pembeli/Pembayaran/Pembayaran';
+import DaftarTransaksi from './Pages/Pembeli/DaftarTransaksi/DaftarTransaksi';
+import DaftarPesanan from './Pages/Penjual/DaftarPesanan/DaftarPesanan';
 
 function App() {
   const router = createBrowserRouter([
+    {
+      path: "*",
+      element: < PageNotFound />,
+    },
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/landingpage",
+      element: <LandingPage />,
+    },
     {
       path: "/login",
       element: <Login />,
@@ -23,12 +42,36 @@ function App() {
       element: <RegisterBuyer />,
     },
     {
-      path: "/admin",
-      element: <Dashboard />,
+      path: "/admin-data-buyer",
+      element: <DataBuyer />,
     },
     {
-      path: "/landingpage",
-      element: <LandingPage />,
+      path: "/admin-data-seller",
+      element: <DataSeller />,
+    },
+    {
+      path: "/admin-verification-data",
+      element: <VerificationData />,
+    },
+    {
+      path: "/pesan-layanan",
+      element: <PesanLayanan />,
+    },
+    {
+      path: "/detail-pesanan/:id",
+      element: <DetailPesanan />,
+    },
+    {
+      path: "/konfirmasi-pembayaran",
+      element: <Pembayaran />,
+    },
+    {
+      path: "/daftar-transaksi",
+      element: <DaftarTransaksi />,
+    },
+    {
+      path: "/daftar-pesanan",
+      element: <DaftarPesanan />,
     }
   ]);
   return (
